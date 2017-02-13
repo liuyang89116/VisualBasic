7a51909d9ab086f76419ab4da2b389225b8a79e8
liuyang89116 <liuyang89116@gmail.com>
2017-02-12 20:59:03 -0600

    Demo4-1

---
+++ lab4/Demo4-1_ListBox/Demo4-1_ListBox/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BookList = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(31, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Select a Title:"
+        '
+        'BookList
+        '
+        Me.BookList.FormattingEnabled = True
+        Me.BookList.Location = New System.Drawing.Point(34, 101)
+        Me.BookList.Name = "BookList"
+        Me.BookList.Size = New System.Drawing.Size(327, 173)
+        Me.BookList.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(133, 306)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(134, 37)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Show Selection"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(403, 379)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.BookList)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BookList As ListBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ lab4/Demo4-1_ListBox/Demo4-1_ListBox/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox(BookList.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BookList.Items.Add("HTML in eays steps")
+        BookList.Items.Add("Effective Java")
+        BookList.Items.Add("Design Pattern")
+        BookList.Items.Add("CSCE101")
+        BookList.Items.Add("Python")
+
+        BookList.Sorted = True
+        BookList.SelectedIndex = 0
+        Me.Text = "We have " & BookList.Items.Count & " books in the list"
+    End Sub
+End Class
