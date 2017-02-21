26279070d98b35bb4a11c4ad985baff935c69232
liuyang89116 <liuyang89116@gmail.com>
2017-02-21 01:24:41 -0600

    Demo5-6, Demo5-7

---
+++ lab5/Demo5-6_Branching/Demo5-6_Branching/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If (Weekday(Now) = vbSaturday) Or (Weekday(Now) = vbSunday) Then
+            Label1.Text = "Relax - it's the weekend"
+        Else
+            Label1.Text = "Let's work! Today is weekday."
+        End If
+    End Sub
+End Class
---
+++ lab5/Demo5-7_SelectCase/Demo5-7_SelectCase/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Select Case Weekday(Now)
+            Case Is = vbMonday
+                Label1.Text = "It's Monday"
+            Case Is = vbTuesday
+                Label1.Text = "It's Tuesday"
+            Case Is = vbWednesday
+                Label1.Text = "It's Wednesday"
+            Case Is = vbThursday
+                Label1.Text = "It's Thursday"
+            Case Is = vbFriday
+                Label1.Text = "It's Friday"
+            Case Else
+                Label1.Text = "It's weekend! Let's party"
+        End Select
+    End Sub
+End Class
