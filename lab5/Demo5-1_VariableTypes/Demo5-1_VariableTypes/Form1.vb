5f3641a610b6aa32c42014dc54a2a9d5d82ffecd
liuyang89116 <liuyang89116@gmail.com>
2017-02-21 00:46:05 -0600

    Demo5-1

---
+++ lab5/Demo5-1_VariableTypes/Demo5-1_VariableTypes/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' create variables
+        Dim flag As Boolean = False
+        Dim str As String = "CSCE101"
+        Dim intNum As Integer = 10
+        Dim doubleNum As Double = 8.8
+
+        ' add variables in listbox
+        ListBox1.Items.Add("flag value is " & flag)
+        ListBox1.Items.Add("str value is " & str)
+        ListBox1.Items.Add("intNum value is " & intNum)
+        ListBox1.Items.Add("doubleNum value is " & doubleNum)
+    End Sub
+End Class
