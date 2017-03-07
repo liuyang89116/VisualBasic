07d35bdfaca7e5760c4f835e85506591447e8816
liuyang89116 <liuyang89116@gmail.com>
2017-03-07 10:46:25 -0600

    Demo7

---
+++ lab7/Demo7-1_BuildingApplication/Demo7-1_BuildingApplication/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Private Sub Clear()
+        Label1.Text = "..."
+        Label2.Text = "..."
+        Label3.Text = "..."
+        Label4.Text = "..."
+        Label5.Text = "..."
+        Label6.Text = "..."
+
+        pickBtn.Enabled = True
+        resetBtn.Enabled = False
+
+        ' start random number generator
+        Randomize()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Clear()
+    End Sub
+
+    Private Sub resetBtn_Click(sender As Object, e As EventArgs) Handles resetBtn.Click
+        Clear()
+    End Sub
+
+    Private Sub pickBtn_Click(sender As Object, e As EventArgs) Handles pickBtn.Click
+        ' declare working variables
+        Dim i, r, tmp, nums(50)
+
+        ' fill elements 1-49 to nums
+        For i = 1 To 49
+            nums(i) = i
+        Next
+
+        ' shuffle the values in elements 1 to 49
+        For i = 1 To 49
+            r = Int(49 * Rnd()) + 1
+            tmp = nums(i)
+            nums(i) = nums(r)
+            nums(r) = tmp
+        Next
+
+        ' display the values
+        Label1.Text = nums(1)
+        Label2.Text = nums(2)
+        Label3.Text = nums(3)
+        Label4.Text = nums(4)
+        Label5.Text = nums(5)
+        Label6.Text = nums(6)
+
+        ' set the button status
+        pickBtn.Enabled = False
+        resetBtn.Enabled = True
+
+    End Sub
+End Class
---
+++ lab7/Demo7-1_BuildingApplication/Demo7-1_BuildingApplication/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Demo7-1_BuildingApplication")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Demo7-1_BuildingApplication")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0f4f64ec-66ef-4476-8733-bc8e1fe1b7dd")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
